b5d287bbaa76fd7a1f70c724ede278d0d8afa269
andriiyan <99918490+andriiyan@users.noreply.github.com>
2022-02-21 10:08:30 +0200

    Task2/configure ioc via xml (#2)
    * Task#2 Configure IoC via XML
    
    * Task#2 Configure IoC via XML [2]
    
    Co-authored-by: Andrii Yanechko <yanechko.andy@gmail.com>

---
+++ src/main/java/com/github/andriiyan/sprongtraining/Main.java
@@ -0,0 +1,20 @@
+package com.github.andriiyan.sprongtraining;
+
+import com.github.andriiyan.sprongtraining.api.facade.BookingFacade;
+import com.github.andriiyan.sprongtraining.api.service.EventService;
+import com.github.andriiyan.sprongtraining.api.service.TicketService;
+import com.github.andriiyan.sprongtraining.api.service.UserService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("configuration.xml");
+        final EventService eventService = context.getBean(EventService.class);
+        final TicketService ticketService = context.getBean(TicketService.class);
+        final UserService userService = context.getBean(UserService.class);
+        final BookingFacade bookingFacade = context.getBean(BookingFacade.class);
+    }
+
+}
